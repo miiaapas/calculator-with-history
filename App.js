7a61abc8aacd5ba03f1c1ebedad5cb70f9430320
Miia Aapajärvi <miia.aapajarvi@haaga-helia.fi>
2021-11-02 20:57:16 +0200

    tehtävä ei ihan valmis

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Button, Value, Alert, TextInput, View, FlatList} from 'react-native';
 
 export default function App() {
-  return (
+
+  const [value, setValue] = useState("");
+  const [secondValue, setSecondValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const[data, setData] = useState([]);
+
+  const calculator = operator => {
+    console.log(value, secondValue, operator);
+    const [numberOne, numberTwo] = [Number(value), Number(secondValue)];
+    switch (operator) {
+      case '+':
+        setResult(value + setSecondValue);
+        break;
+      case '-':
+        setResult(value - secondValue);
+        break;
+    }
+    setValue('');
+    setSecondValue;
+  }
+
+
+  const addData = () => {
+    setData([...data, {key: result}]);
+    setResult('');
+  }
+        
+  return(
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Result: {result}</Text>
+      <TextInput
+        style={{width:200, borderColor:'gray', borderWidth:1}}
+        keyboardType='numeric'
+        onChangeText={text => setValue(text)}
+        value={value}        
+      />
+      <TextInput
+        style={{width:200, borderColor:'gray', borderWidth:1}}
+        keyboardType='numeric'
+        onChangeText={text => setSecondValue(text)}
+        value={secondValue}        
+      />
+
+      <View style={styles.buttons}>
+      <Button onPress={() => calculator('+')} onPress={addData} title="+"/>
+      <Button onPress={() => calculator('-')} onPress={addData} title="-"/> 
+      </View>
+      <FlatList
+        data={data}
+        renderItem = {({item}) =>
+          <Text>{item.key}
+          </Text>}
+        />
+      
+  </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 100,
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+
+   input: {
+     
+    width:200, 
+    borderColor:'gray', 
+    borderWidth:1,
+    justifyContent: 'center',
+   },
+   
+   buttons: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection:'row'
+   }
+
 });
+
